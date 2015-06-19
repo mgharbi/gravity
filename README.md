698c7a8f41ea4f87d2541c17388ceefa721c8e0e
Michael Gharbi <gharbi@mit.edu>
2015-06-18 23:50:01 -0400

    adds basic controls

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 Gravity
 =======
 
+Dependencies
+------------
+
+* Irrlicht graphics engine `brew install irrlicht`
+* Bullet physics engine `brew install bullet`
+* Boost
+
+
 Install
 -------
 
-On OSX with Xcode:
+* On OSX with Xcode:
 
     cd macbuild
     cmake -G "Xcode" ..
 
 Open the Xcode project, all set !
+
+* On OSX with make
+
+    cd build
+    cmake ..
+    make
+    open ../bin/gravity.app
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <irrlicht.h>
+#include <boost/filesystem.hpp>
 
 #include "GREventHandler.h"
 #include "GRResourceLoader.h"
-#include <boost/filesystem.hpp>
 
 #include <iostream>
 
@@ -31,6 +31,7 @@ int main()
     params.Vsync           = false;
     params.Fullscreen      = false;
     params.EventReceiver   = &receiver;
+    params.WindowSize = core::dimension2du(480,640);
     IrrlichtDevice *device = createDeviceEx(params);
 
     if (!device)
@@ -43,48 +44,70 @@ int main()
     ISceneManager* smgr     = device->getSceneManager();
     IGUIEnvironment* guienv = device->getGUIEnvironment();
 
+    // IO
     fs::path spaceship_path = get_resource_path("spaceship.zip");
-
+    fs::path earth_path     = get_resource_path("Earth.zip");
     device->getFileSystem()->addFileArchive(spaceship_path.c_str());
+    device->getFileSystem()->addFileArchive(earth_path.c_str());
 
-    scene::IMeshSceneNode* node = smgr->addMeshSceneNode(smgr->getMesh("spaceship.obj"),0,-1,core::vector3df(0.0f,0.0f,0.0f),
-            core::vector3df(0.f,0.f,0.f),core::vector3df(.1f,.1f,.1f));
+    // Scene setup
+    scene::IMeshSceneNode* spaceship = smgr->addMeshSceneNode(smgr->getMesh("spaceship.obj"),0,-1,core::vector3df(0.0f,0.0f,-10.0f),
+            core::vector3df(0.f,0.f,0.f),core::vector3df(.01f,.01f,.01f));
+    scene::IMeshSceneNode* earth = smgr->addMeshSceneNode(smgr->getMesh("Earth.obj"),0,-1,core::vector3df(0.0f,0.0f,10.0f),
+            core::vector3df(0.f,0.f,0.f),core::vector3df(10.0f,10.0f,10.0f));
 
-    node->setMaterialFlag(video::EMF_LIGHTING, true);
+    spaceship->setMaterialFlag(video::EMF_LIGHTING, false);
+    earth->setMaterialFlag(video::EMF_LIGHTING, false);
 
-    smgr->setAmbientLight(video::SColorf(0.3,0.3,0.3,1));
-    ILightSceneNode* light1 = smgr->addLightSceneNode( 0, core::vector3df(0,0,100), video::SColorf(0.3f,0.3f,0.3f), 100.0f, 1 ); 
+    // smgr->setAmbientLight(video::SColorf(0.3,0.3,0.3,1));
+    // ILightSceneNode* light1 = smgr->addLightSceneNode( 0, core::vector3df(0,500,0), video::SColorf(0.3f,0.3f,0.3f), 200.0f, 1 ); 
 
-    smgr->addCameraSceneNodeFPS();
+
+    // View setup
+    smgr->addCameraSceneNode(
+        0,
+        core::vector3df(0, 20, -20),
+        core::vector3df(0, 0, 0)
+    );
 
     device->getCursorControl()->setVisible(false);
 
-    int lastFPS = -1;
+    u32 then = device->getTimer()->getTime();
 
+    // This is the movemen speed in units per second.
+    const f32 MOVEMENT_SPEED = 20.f;
+
+    // Main game loop
     while(device->run())
     {
         if(receiver.IsKeyDown(irr::KEY_ESCAPE)) {
             printf("out\n");
             break;
         }
+
+        const u32 now = device->getTimer()->getTime();
+        const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
+        then = now;
+
+        // Spaceship controls
+        core::vector3df nodePosition = spaceship->getPosition();
+        if(receiver.IsKeyDown(irr::KEY_KEY_W))
+            nodePosition.Z += MOVEMENT_SPEED * frameDeltaTime;
+        else if(receiver.IsKeyDown(irr::KEY_KEY_S))
+            nodePosition.Z -= MOVEMENT_SPEED * frameDeltaTime;
+        if(receiver.IsKeyDown(irr::KEY_KEY_A))
+            nodePosition.X -= MOVEMENT_SPEED * frameDeltaTime;
+        else if(receiver.IsKeyDown(irr::KEY_KEY_D))
+            nodePosition.X += MOVEMENT_SPEED * frameDeltaTime;
+
+        spaceship->setPosition(nodePosition);
+
         if (device->isWindowActive())
         {
             driver->beginScene(true, true, video::SColor(255,200,200,200));
+
             smgr->drawAll();
             driver->endScene();
-
-            int fps = driver->getFPS();
-
-            if (lastFPS != fps)
-            {
-                core::stringw str = L"Irrlicht Engine - Quake 3 Map example [";
-                str += driver->getName();
-                str += "] FPS:";
-                str += fps;
-
-                device->setWindowCaption(str.c_str());
-                lastFPS = fps;
-            }
         }
         else {
             device->yield();
